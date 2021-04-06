3d84fbaec4e87eba8aca0d8be478289daa2e9a18
Mark Paluch <mpaluch@vmware.com>
2021-04-06 09:34:57 +0200

    Remove parameter binding validation.
    ParametrizedMssqlStatement no longer performs an exhaustive check whether variables without an assignment are provided through bind(…). The identification mechanism just checks whether a at-variable is declared but not how it is used or whether its value is already assigned. We now rely on the runtime verification by running the query and await server-side errors instead of attempting to validate bindings on the client side.
    
    [resolves #197]
    
    Signed-off-by: Mark Paluch <mpaluch@vmware.com>

--- src/main/java/io/r2dbc/mssql/ParametrizedMssqlStatement.java
+++ src/main/java/io/r2dbc/mssql/ParametrizedMssqlStatement.java
@@ -100,15 +100,12 @@ final class ParametrizedMssqlStatement extends MssqlStatementSupport implements 
     public ParametrizedMssqlStatement add() {
 
         assertNotExecuted();
-        this.bindings.validate(this.parsedQuery.getParameters());
         this.bindings.finish();
         return this;
     }
 
     @Override
     public Flux<MssqlResult> execute() {
-
-        this.bindings.validate(this.parsedQuery.getParameters());
 
         int effectiveFetchSize = getEffectiveFetchSize();
         return Flux.defer(() -> {
@@ -612,20 +609,6 @@ final class ParametrizedMssqlStatement extends MssqlStatementSupport implements 
 
         private Binding current;
 
-        public void validate(List<ParsedParameter> parameters) {
-
-            for (Binding binding : this.bindings) {
-
-                Map<String, Encoded> bindingset = binding.getParameters();
-
-                for (ParsedParameter parameter : parameters) {
-
-                    if (!bindingset.containsKey(parameter.getName())) {
-                        throw new IllegalStateException("No parameter binding for " + parameter.getName());
-                    }
-                }
-            }
-        }
 
         private void finish() {
             this.current = null;
