5868d7354c8701bf4b17c63c9570062bacd9d0be
Mark Paluch <mpaluch@vmware.com>
2021-04-06 10:41:40 +0200

    Update copyright years to 2021
    Signed-off-by: Mark Paluch <mpaluch@vmware.com>

--- src/main/java/io/r2dbc/mssql/QueryMessageFlow.java
+++ src/main/java/io/r2dbc/mssql/QueryMessageFlow.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2020 the original author or authors.
+ * Copyright 2018-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,6 +18,7 @@ package io.r2dbc.mssql;
 
 import io.r2dbc.mssql.client.Client;
 import io.r2dbc.mssql.message.Message;
+import io.r2dbc.mssql.message.TransactionDescriptor;
 import io.r2dbc.mssql.message.token.DoneToken;
 import io.r2dbc.mssql.message.token.SqlBatch;
 import io.r2dbc.mssql.util.Assert;
